b9391746204e112674b432e5c85617e91b68aa67
favas8606 <mailto:favaskzp@gmail.com>
2023-05-20 20:36:39 +0530

    Add project files.

---
+++ Fortnite Pinger/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Fortnite_Pinger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(230, 145);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::Fortnite_Pinger.Properties.Resources.textNotReady;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(85, 125);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(65, 15);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::Fortnite_Pinger.Properties.Resources.iconNotReady;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(98, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(9, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Made With 💚 | SALIH101";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(82, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 10);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "CLICK TO PLAY\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(230, 145);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Samsung Sharp Sans", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Fortnite Pinger/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Configuration;
+using System.ComponentModel;
+
+
+namespace Fortnite_Pinger
+{
+    public partial class Form1 : Form
+    {
+        private int pingCount;
+        private String targetDomain;
+        private int pingInterval;
+        private Timer timer;
+        private string fortniteURL;
+        private int latencyLimit;
+
+        private BackgroundWorker pingWorker;
+
+        private bool isDragging;
+        private Point lastCursorPosition;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer = new Timer();
+            ReadConfig();
+
+            pingWorker = new BackgroundWorker();
+            pingWorker.WorkerSupportsCancellation = true;
+            pingWorker.DoWork += PingWorker_DoWork;
+            pingWorker.RunWorkerAsync();
+
+            timer.Interval = pingInterval;
+            timer.Start();
+
+
+            panel1.MouseDown += Form1_MouseDown;
+            panel1.MouseMove += Form1_MouseMove;
+            panel1.MouseUp += Form1_MouseUp;
+
+            panel1.MouseEnter += Form1_MouseEnter;
+            panel1.MouseLeave += Form1_MouseLeave;
+            pictureBox1.MouseEnter += Form1_MouseEnter;
+            pictureBox1.MouseLeave += Form1_MouseEnter;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDragging = true;
+            lastCursorPosition = e.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                int deltaX = e.X - lastCursorPosition.X;
+                int deltaY = e.Y - lastCursorPosition.Y;
+
+                this.Location = new Point(this.Location.X + deltaX, this.Location.Y + deltaY);
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+
+        //Mouse Hover Opacity Changes
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            this.Opacity = 1.0;
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            this.Opacity = 0.3;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        { 
+            Process.Start(fortniteURL);
+
+        }
+
+        private void ReadConfig()
+        {
+            try
+            {
+                pingCount = int.Parse(ConfigurationManager.AppSettings["PING_COUNT"]);
+                targetDomain = ConfigurationManager.AppSettings["TARGET_DOMAIN"];
+                pingInterval = int.Parse(ConfigurationManager.AppSettings["PING_INTERVAL"]);
+                fortniteURL = ConfigurationManager.AppSettings["FORTNITE_URL"];
+                latencyLimit = int.Parse(ConfigurationManager.AppSettings["LATENCY_LIMIT"]);
+            }
+            catch (Exception ex)
+            {
+                // Handle any errors when reading config values
+                MessageBox.Show("Error reading configuration: " + ex.Message);
+                Environment.Exit(0); // Close the application
+            }
+        }
+
+        private void PingWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (!pingWorker.CancellationPending)
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "ping";
+                process.StartInfo.Arguments = $"-n {pingCount} {targetDomain}";
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+
+                Match match = Regex.Match(output, @"Average = (\d+)ms");
+                if (match.Success)
+                {
+                    int avgLatency = int.Parse(match.Groups[1].Value);
+                    pictureBox1.BackgroundImage = avgLatency < latencyLimit ? Properties.Resources.iconReady : Properties.Resources.iconNotReady;
+                    pictureBox2.BackgroundImage = avgLatency < latencyLimit ? Properties.Resources.textReady : Properties.Resources.textNotReady;
+
+                    // Update UI on the main thread
+                    this.Invoke((MethodInvoker)delegate {
+                        label1.Text = $"Average Latency: {avgLatency} ms";
+                 
+                    });
+                }
+
+                // Wait for the specified interval
+                System.Threading.Thread.Sleep(pingInterval*1000);
+            }
+        }
+
+    }
+
+}
